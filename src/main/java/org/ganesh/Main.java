da8b83ac094dfcd63a1d9f7b40fb02b3bb84344b
Ganesh Renuse <ganesh21renuse3@gmail.com>
2025-08-11 20:44:04 +0530

    Updated Files with some minor changes in database properties.

--- src/main/java/org/ganesh/Main.java
+++ src/main/java/org/ganesh/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         Students studentObject = new Students();
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! Welcome to the Student Management System");
 
         studentObject.InsertDetails();
         studentObject.ShowDetails();
